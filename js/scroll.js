ba679094184c79d559fbf4ad28577a03a40c36d5
anuar <anuarshaidenov@gmail.com>
2020-06-19 19:17:33 +0300

    responsive on all devices (almost)

---
+++ js/scroll.js
@@ -0,0 +1,32 @@
+function smoothScroll(target, duration) {
+    const target = document.querySelector(target);
+    const targetPosition = target.getBoundingClientRect().top;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+        let run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation)
+}
+
+menuMobileLinks = document.querySelectorAll("#menu-mobile ul li a");
+
+menuMobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        alert('clicked');
+    });
+})
