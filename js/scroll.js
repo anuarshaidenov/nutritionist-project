602734c744f51281cb30e0b5d99ef7a972d340eb
anuar <anuarshaidenov@gmail.com>
2020-06-20 17:09:35 +0300

    desktop nav scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,14 @@
-const links = document.querySelectorAll('#menu-mobile li');
+const linksMobile = document.querySelectorAll('#menu-mobile li');
 const sectionProcess = document.querySelector('.section-process');
 const sectionTestimonials = document.querySelector('.section-testimonials');
 const sectionSignup = document.querySelector('.section-signup');
 const buttons = document.querySelectorAll('#scroll');
+const links = document.querySelectorAll('.nav-link');
 
 const sections = [sectionProcess, sectionTestimonials, sectionSignup];
 
-for(let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', () => {
+for(let i = 0; i < linksMobile.length; i++) {
+    linksMobile[i].addEventListener('click', () => {
         sections[i].scrollIntoView({ behavior: 'smooth', block: 'center'});
         menuImg.src = "./svg/menu.svg";
         menu.classList.remove('mobile-show');
@@ -22,3 +23,10 @@ buttons.forEach((button) => {
         sectionSignup.scrollIntoView({ behavior: 'smooth', block: 'center'});
     });
 });
+
+
+for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', () => {
+        sections[i].scrollIntoView({ behavior: 'smooth', block: 'center'});
+    });
+}
